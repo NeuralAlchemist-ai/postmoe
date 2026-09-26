fcc62c42e80147bbbab66288d8698a742005de31
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-09-26 18:01:51 +0200

    refactor: update pipeline arguments, add target rank to SVD compression, and remove weight reshaping

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,26 +1,28 @@
 import argparse
 
-from torchgen import model
 from pipeline import Pipeline
-from postmoe.config import User_Config
+from postmoe.config import Config
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_name", type=str, default="gpt2")
-parser.add_argument("--rope_dim", type=int, default=64)
-parser.add_argument("--data_path", type=str, default="data.txt")
-parser.add_argument("--data_config", type=str, default="wikitext-2-raw-v1")  # Added argument for dataset configuration
+parser.add_argument("--model_name", type=str, default="Qwen/Qwen2-0.5B")
+parser.add_argument("--rope_dim", type=int, default=32)
+parser.add_argument("--dataset_name", type=str, default="Salesforce/wikitext")
+parser.add_argument("--dataset_config", type=str, default="wikitext-2-raw-v1")
+parser.add_argument("--split", type=str, default="test[:100]")
 args = parser.parse_args()
 
-config = User_Config(
+config = Config(
     model_name=args.model_name,
     rope_dim=args.rope_dim,
-    data_path=args.data_path
+    dataset_name=args.dataset_name,
+    dataset_config=args.dataset_config,
+    split=args.split,
 )
 
 pipeline = Pipeline(config)
 pipeline.convert()
 
-for layer in pipeline.model.model.layers:
-    layer.self_attn = pipeline.custom_MLA_attention(layer.self_attn.o_proj)
+for i, layer in enumerate(pipeline.model.model.layers):
+    layer.self_attn = pipeline.custom_MLA_attention(i, layer.self_attn.o_proj)
 
 pipeline.evaluate()
